10eee2eff29f1985890006c4ffa5172585d1eea8
DanielMartinezM <ldanielmartinezm@gmail.com>
2016-05-05 00:38:16 -0400

    Added files via upload

---
+++ index.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Compartir Redes Sociales</title>
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.6.2/css/font-awesome.min.css">
+<link rel="stylesheet" type="text/css" href="css.css">
+</head>
+<body>
+
+
+
+
+<ul>
+	<li>
+		<a href="https://www.facebook.com/sharer/sharer.php?u=http://looknoticias.com/adelgazar-comiendo-es-posible/" class="facebook" target="_blank">
+   			<i class="fa fa-facebook" aria-hidden="true" style="font-size: 30px; color:#2274B9;"></i>
+
+		
+		<p class="social">COMPARTIR</p></a>
+    </li>
+<li>
+	<a href="https://twitter.com/intent/tweet?url=http://looknoticias.com/adelgazar-comiendo-es-posible/&text=TITULO %20por%20@Diwallia" class="twitter" target="_blank">
+    	<i class="fa fa-twitter" aria-hidden="true" style="font-size: 30px; color:#4D9ED8;"></i>
+	
+	<p class="social">COMPARTIR</p></a>
+</li>
+<li>
+	<a href="https://plus.google.com/share?url=http://looknoticias.com/adelgazar-comiendo-es-posible/" class="googleplus" target="_blank">
+   <i class="fa fa-google-plus" aria-hidden="true" style="font-size: 30px; color:#999999;"></i>
+	
+	<p class="social">COMPARTIR</p></a>
+</li>
+<li>
+
+<a href="https://pinterest.com/pin/create/button/?url=http://looknoticias.com/adelgazar-comiendo-es-posible/&media=http://looknoticias.com/wp-content/uploads/2016/04/salad-374173_1280-660x400.jpg" class="pinterest" target="_blank">
+    <i class="fa fa-pinterest-p" aria-hidden="true" style="font-size: 30px; color:#BC0D1E;"></i>
+
+	<p class="social">COMPARTIR</p></a>
+</li>
+<li>
+	<a href="https://www.linkedin.com/shareArticle?mini=true&url=http%3A//looknoticias.com/adelgazar-comiendo-es-posible/&title=adelgazar&summary=&source=" target="_blank"><i class="fa fa-linkedin" aria-hidden="true" style="font-size: 30px; color:#2274B9;"></i>
+	
+	<p class="social">COMPARTIR</p></a>
+</li>
+</ul>
+
+
+
+</body>
+</html>
