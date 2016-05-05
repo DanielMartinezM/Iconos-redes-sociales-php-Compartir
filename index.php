03c3b026faa0600bf05ff1760260f9a549f689c1
DanielMartinezM <ldanielmartinezm@gmail.com>
2016-05-05 06:01:10 -0400

    Added files via upload

--- index.php
+++ index.php
@@ -12,33 +12,37 @@
 
 <ul>
 	<li>
-		<a href="https://www.facebook.com/sharer/sharer.php?u=http://looknoticias.com/adelgazar-comiendo-es-posible/" class="facebook" target="_blank">
+								<!-- UTILIZAR SOLO LA ETIQUETA <a> DEL SIGUIENTE CODIGO-->
+		<a href="https://www.facebook.com/sharer/sharer.php?u=http://cooltasti.co/relaciones/lograr-la-felicidad-plena-pareja/" class="facebook" target="_blank"> <!--CODIGO NECESARIO PARA COMPARTIR EN FACEBOOK-->
    			<i class="fa fa-facebook" aria-hidden="true" style="font-size: 30px; color:#2274B9;"></i>
 
 		
 		<p class="social">COMPARTIR</p></a>
     </li>
-<li>
-	<a href="https://twitter.com/intent/tweet?url=http://looknoticias.com/adelgazar-comiendo-es-posible/&text=TITULO %20por%20@Diwallia" class="twitter" target="_blank">
+<li>							<!-- UTILIZAR SOLO LA ETIQUETA <a> DEL SIGUIENTE CODIGO-->
+	<a href="https://twitter.com/intent/tweet?url=http://cooltasti.co/relaciones/lograr-la-felicidad-plena-pareja/&text=Cómo lograr la felicidad plena en pareja %20por%20@Cooltasti.co" class="twitter" target="_blank"> <!--CODIGO NECESARIO PARA COMPARTIR EN TWITTER-->
     	<i class="fa fa-twitter" aria-hidden="true" style="font-size: 30px; color:#4D9ED8;"></i>
 	
 	<p class="social">COMPARTIR</p></a>
 </li>
-<li>
-	<a href="https://plus.google.com/share?url=http://looknoticias.com/adelgazar-comiendo-es-posible/" class="googleplus" target="_blank">
+<li>							<!-- UTILIZAR SOLO LA ETIQUETA <a> DEL SIGUIENTE CODIGO-->
+	<a href="https://plus.google.com/share?url=http://cooltasti.co/relaciones/lograr-la-felicidad-plena-pareja/" class="googleplus" target="_blank"> 							<!--CODIGO NECESARIO PARA COMPARTIR EN GOOGLE + -->
    <i class="fa fa-google-plus" aria-hidden="true" style="font-size: 30px; color:#999999;"></i>
 	
 	<p class="social">COMPARTIR</p></a>
 </li>
 <li>
-
-<a href="https://pinterest.com/pin/create/button/?url=http://looknoticias.com/adelgazar-comiendo-es-posible/&media=http://looknoticias.com/wp-content/uploads/2016/04/salad-374173_1280-660x400.jpg" class="pinterest" target="_blank">
+								<!-- UTILIZAR SOLO LA ETIQUETA <a> DEL SIGUIENTE CODIGO-->
+<a href="https://pinterest.com/pin/create/button/?url=http://cooltasti.co/relaciones/lograr-la-felicidad-plena-pareja/&media=http://cooltasti.co/wp-content/uploads/2016/03/C%C3%B3mo-lograr-la-felicidad-plena-en-pareja-.jpg" class="pinterest" target="_blank">
+								<!--CODIGO NECESARIO PARA COMPARTIR EN PINTEREST -->
     <i class="fa fa-pinterest-p" aria-hidden="true" style="font-size: 30px; color:#BC0D1E;"></i>
 
 	<p class="social">COMPARTIR</p></a>
 </li>
-<li>
-	<a href="https://www.linkedin.com/shareArticle?mini=true&url=http%3A//looknoticias.com/adelgazar-comiendo-es-posible/&title=adelgazar&summary=&source=" target="_blank"><i class="fa fa-linkedin" aria-hidden="true" style="font-size: 30px; color:#2274B9;"></i>
+<li>							<!-- UTILIZAR SOLO LA ETIQUETA <a> DEL SIGUIENTE CODIGO-->
+	<a href="https://www.linkedin.com/shareArticle?mini=true&url=http://cooltasti.co/relaciones/lograr-la-felicidad-plena-pareja/&title=Cómo lograr la felicidad plena en pareja" target="_blank">
+								<!--CODIGO NECESARIO PARA COMPARTIR EN LINKEDIN -->
+	<i class="fa fa-linkedin" aria-hidden="true" style="font-size: 30px; color:#2274B9;"></i>
 	
 	<p class="social">COMPARTIR</p></a>
 </li>
